76b2977b678c66f8b0e68eb31c03956bf1cefe5c
Amir Asaadi <a.asadi94@gmail.com>
2018-01-18 15:19:32 -0800

    creating users tabel

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
-<!doctype html>
+<?php
+	if(isset($_POST)) //if submit button clicked
+	{
+		$firstName = $_POST['fname'];
+		$lastName = $_POST['lname'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		$passwordConfirm = $_POST['passwordConfirm'];
+
+		$image = $_FILES['image']['name']; //second is name of file
+		$tmp_image= $_FILES['image']['tmp_name']; //for uploading
+		$imageSize = $_FILES['image']['size']; //size of file
+
+		echo $firstName;
+	}	
+?>
+
+
+<!Doctype html>
 <html>
 	<head>
+	
 		<meta charset="utf-8">
 		<title>صحفه ثبت نام</title>
 		<link rel="stylesheet" href="css/styles.css"/> 
@@ -9,7 +28,7 @@
 		<div id="wrapper">
 			
 			<div id="formDiv">
-				<form method="POST" action="process.php" enctype="multipart/form-data">
+				<form method="POST" action="index.php" enctype="multipart/form-data">
 				
 					<label> نام</label><br/>
 					<input type="text" name="fname" /><br/><br/>
@@ -21,17 +40,20 @@
 					<input type="text" name="email" /><br/><br/>
 					
 					<label>کلمه عبور</label><br/>
-					<input type="password" name="password" /><br/><br/>
+					<input type="password" name="password" /><br/><br
+					
+					<label>تکرار کلمه عبور</label><br/>
+					<input type="password" name="passwordConfirm" /><br/><br/> 
 					
 					<label>تصویر پروفایل</label><br/>
 					<input type="file" name="image" /><br/><br/>
 					
-					<label> ارسال</label><br/>
-					<input type="submit" name="submit" /><br/><br/>
+					
+					<input type="submit" name="submit" />
+					
 				</form>
 			</div>
 			
-		</div>
-		
+		</div>	
 	</body>
 </html>
